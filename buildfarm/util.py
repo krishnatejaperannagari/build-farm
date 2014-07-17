a5fbe2928dacb5a70d20cdf4afd2fcf321a19033
krishnatejaperannagari <krishnatejaperannagari@gmail.com>
2014-07-17 19:48:05 +0530

    deleting submodule and adding the main samba one and using submodule
    Signed-off-by: krishnatejaperannagari <krishnatejaperannagari@gmail.com>

--- buildfarm/util.py
+++ buildfarm/util.py
@@ -17,7 +17,6 @@
 #   along with this program; if not, write to the Free Software
 #   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-import urllib2
 import re
 
 def load_list(fname):
@@ -43,9 +42,13 @@ def FileLoad(filename):
     finally:
         f.close()
 
-def UrlLoad(url):
-    """reads url parses the relatuve links"""
-    links = urllib2.urlopen(url)
+def LoadAndParse(filename):
+    """loads file and changes the links to suit buildfarm""" 
+    f = open(filename, 'r')
+    try:
+        links = f.read()
+    finally:
+        f.close()
     modifiedlinks = ""
     for line in links:
         line = re.sub('href="/samba', 'href="http://www.samba.org/samba' ,line)
